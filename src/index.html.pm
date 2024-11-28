544c21249105a8aa6f9bc6c7324c9b4bc6e1bead
Ashton Wiersdorf <mail@wiersdorf.dev>
2024-11-27 23:53:40 -0700

    Let there be a site—and there was a site

---
+++ src/index.html.pm
@@ -0,0 +1,71 @@
+#lang pollen
+
+@sacrament-meeting-header{Salt Lake Married Student 9th Ward}
+
+@(meeting-org #:conducting "Bishop Humpherys"
+              #:presiding "Bishop Humpherys"
+              #:organist "Brother Hansen"
+              #:chorister "Brother Parish")
+
+
+@section{Program}
+
+@opening-hymn{68}
+
+@opening-prayer{Brother Whomever}
+
+@event{Ward & Stake Business}
+
+@sacrament-hymn{194}
+
+@event{Administration of the Sacrament}
+
+@talk{Sister Soandso}
+
+@talk{Brother Soandso}
+
+@hymn[#:term "Rest Hymn"]{100}
+
+@talk{Another Bloke}
+
+@event{Announcements}
+
+@closing-hymn{201}
+
+@closing-prayer{Sister Whomever}
+
+
+@section{Announcements}
+
+@announcement["Ward Slack workspace"]{
+Our ward uses a Slack workspace to send out announcements about activites, as well as to coordinate service. Please join with @link["https://join.slack.com/t/saltlakemarri-0vv2943/shared_invite/zt-26qlfk5n4-iUzhY3Mlg6Tky6v1JY9qPw"]{this invite link}.
+}
+
+@announcement["New member form"]{
+If you are new to the ward, please help us move your records in by filling out @link["https://forms.gle/bVHiqHTurYz2GYcs5"]{this short form}.
+}
+
+@announcement["Tithing settlement"]{
+Sign up for tithing settlement on Slack or at @link["https://m.signupgenius.com/#!/showSignUp/10C0A4EA8A729A7FBC07-51852185-tithing"]{this link}.
+}
+
+@announcement["Christmas program choir"]{
+The 9th and 14th wards are combined for Christmas. Practices for a choir happen after church each Sunday in our chapel.
+}
+
+@announcement["Picture for the ward bulletin"]{
+We would love to have a picture of your family for our ward bulletin! Please either bring us a picture yourself, or upload one to @link["https://forms.gle/zDnMhWEGNxgcGupN9"]{this form} and we'll print it for you.
+}
+
+
+@section{Upcoming Events}
+
+@calendar-event["Ward Christmas party" #:date "2024-12-06" #:time "18:00"]{
+Invite your friends to the ward Christmas party! There will be good food and family-friendly entertainment.
+}
+
+@calendar-event["First Presidency Christmas devotional" #:date "2024-12-08" #:time "18:00"]{The First Presidency invites Church members and friends to participate in the First Presidency’s Christmas devotional broadcast.}
+
+@calendar-event["Ward temple night" #:date "3rd Wednesday and Friday each month" #:time "19:00"]{
+Ward temple nights are the 3rd Wednesday and Friday of every month at 7:00 PM at the Bountiful temple. Sign up to make an appointment at @link["https://www.churchofjesuschrist.org/temples/schedule/appointment?lang=eng"]{this link}.
+}
---
+++ src/pollen.rkt
@@ -0,0 +1,144 @@
+#lang racket/base
+
+(require racket/list pollen/decode txexpr gregor pollen/tag pollen/unstable/typography)
+(require "hymn_names.rkt")
+(provide (all-defined-out))
+
+(module setup racket/base
+  (provide (all-defined-out))
+  (define command-char #\@))
+
+(define (program-header unit-name [title "Sacrament Meeting Program"])
+  `(div (h1 ((class "header")) ,title)
+        (h2 ((class "header")) ,unit-name)))
+
+(define (sacrament-meeting-header unit-name)
+  (program-header unit-name "Sacrament Meeting Program"))
+
+(define (meeting-org #:presiding [presiding #f]
+                     #:conducting [conducting #f]
+                     #:chorister [chorister #f]
+                     #:pianist [pianist #f]
+                     #:organist [organist #f])
+  `(div ((class "meeting-org"))
+        (div ((class "leadership"))
+             ,(when presiding
+                `(div (h5 "Presiding")
+                      ,presiding))
+             ,(when conducting
+                `(div (h5 "Conducting")
+                      ,conducting)))
+        (div ((class "music"))
+             ,(when chorister
+                `(div (h5 "Chorister")
+                      ,chorister))
+             ,(when pianist
+                `(div (h5 "Pianist")
+                      ,pianist))
+             ,(when organist
+                `(div (h5 "Organist")
+                      ,organist)))))
+
+(define section (default-tag-function 'h3))
+
+(define event (default-tag-function 'h4))
+
+;; events with a person attached to them
+(define (person-event name #:term term)
+  `(div ((class "program-event prayer"))
+        (h5 ,term)
+        ,name))
+
+(define (opening-prayer name #:term [term "Invocation"])
+  (person-event name #:term term))
+(define (closing-prayer name #:term [term "Benediction"])
+  (person-event name #:term term))
+(define (talk name #:term [term "Speaker"])
+  (person-event name #:term term))
+
+(define (opening-hymn name-or-number #:term [term "Opening Hymn"])
+  (hymn name-or-number #:term term))
+(define (closing-hymn name-or-number #:term [term "Closing Hymn"])
+  (hymn name-or-number #:term term))
+(define (sacrament-hymn name-or-number #:term [term "Sacrament Hymn"])
+  (hymn name-or-number #:term term))
+
+(define (render-hymn number name [term "Hymn"])
+  `(div ((class "program-event hymn"))
+        (h5 ,term)
+        (span ((class "hymn-number")) ,(number->string number))
+        (span ((class "hymn-name")) ,name)))
+
+(define (make-hymn-from-name name term)
+  (let ([num-name (findf (λ (x) (equal? (string-locale-downcase (cdr x))
+                                        (string-locale-downcase name))) hymn-number-name)])
+    (render-hymn (car num-name) (cdr num-name) term)))
+(define (make-hymn-from-number num term)
+  (let ([name (assoc num hymn-number-name)])
+    (render-hymn num (cdr name) term)))
+
+(define (hymn name-or-number #:term [term "Hymn"])
+  (let ([m (regexp-match "^([0-9]+)$" name-or-number)])
+    (if m
+        (make-hymn-from-number (string->number (cadr m)) term)
+        (make-hymn-from-name name-or-number term))))
+
+(define (announcement header . body-text)
+  `(div ((class "announcement"))
+        (div ((class "announcement-header")) ,header)
+        (div ((class "announcement-description"))
+             ,@body-text)))
+
+(define (try-dt-parse f str)
+  (with-handlers ([exn:gregor:parse? (λ (_) #f)])
+    (f str)))
+
+(define (calendar-event title #:date the-date #:time [the-time ""] . description)
+  (let* ([parsed-date (try-dt-parse iso8601->date the-date)]
+         [parsed-time (try-dt-parse iso8601->time the-time)]
+         [full-string (cond [(and parsed-date parsed-time)
+                             (string-append (~t parsed-date "EEEE, MMMM d, y")
+                                            " at "
+                                            (~t parsed-time "h:mm a"))]
+                            [parsed-date
+                             (string-append
+                              (~t parsed-date "EEEE, MMMM d, y")
+                              (if (not (equal? the-time "")) (format " at ~a" the-time) ""))]
+                            [parsed-time
+                             (string-append
+                              the-date " at " (~t parsed-time "h:mm a"))]
+                            [else
+                             (format "~a ~a" the-date the-time)])])
+    `(div ((class "calendar-event"))
+          (div ((class "event-metadata"))
+               (span ((class "event-title")) ,title)
+               (span ((class "event-datetime")) ,full-string))
+          (div ((class "event-description"))
+          ,@description))))
+
+(define (link url text) `(a ((href ,url)) ,text))
+
+(define (root . elements)
+  (txexpr 'root empty
+          (decode-elements elements
+                           #:txexpr-elements-proc decode-paragraphs
+                           #:string-proc (compose1
+                                          #;make-quotes-hangable
+                                          smart-quotes
+                                          smart-dashes
+                                          smart-ellipses))))
+
+;; snarfed from https://docs.racket-lang.org/pollen-tfl/_pollen_rkt_.html#%28def._%28%28lib._pollen-tfl%2Fpollen..rkt%29._make-quotes-hangable%29%29
+#;(define (make-quotes-hangable str)
+  (define substrs (regexp-match* #px"\\s?[“‘]" str #:gap-select? #t))
+  (if (= (length substrs) 1)
+      (car substrs)
+      (cons 'quo (append-map (λ (str)
+                               (let ([strlen (string-length str)])
+                                 (if (> strlen 0)
+                                     (case (substring str (sub1 strlen) strlen)
+                                       [("‘") (list '(squo-push) `(squo-pull ,str))]
+                                       [("“") (list '(dquo-push) `(dquo-pull ,str))]
+                                       [else (list str)])
+                                     (list str)))) substrs))))
+
