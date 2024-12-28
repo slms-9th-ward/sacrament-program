4308ee6c5260408d6fe2363e5288f94b1e9086d4
Bkhump <benkimhumpherys@hotmail.com>
2024-12-28 11:15:32 -0700

    Update index.html.pm

--- src/index.html.pm
+++ src/index.html.pm
@@ -7,10 +7,10 @@
 @; from the fast-and-testimony-header line and add one to the
 @; sacrament-meeting-header line, and then reverse it for the next week.
 
-@sacrament-meeting-header{Salt Lake Married Student 9th Ward}
-@;fast-and-testimony-header{Salt Lake Married Student 9th Ward}
+@;sacrament-meeting-header{Salt Lake Married Student 9th Ward}
+@fast-and-testimony-header{Salt Lake Married Student 9th Ward}
 
-@banner{christmas_picture.png}
+banner{christmas_picture.png}
 
 @welcome-blurb{Visitors and members alike, welcome to the Church of Jesus Christ of Latter-day Saints!}
 
@@ -18,33 +18,35 @@
 @; case sensitive!
 
 @(meeting-org
-    #:conducting "Brother Jensen"
+    #:conducting "Bishop Humpherys"
     #:presiding "Bishop Humpherys"
     #:chorister "Sister Evans"
-    #:pianist "Brother Henson"
+    #:pianist "Brother Wiersdorf"
     )  @; make sure the close paren stays!
 
 @section{Program}
 
-@opening-hymn{202}
+@opening-hymn{203}
 
-@opening-prayer{Brother Jaszkowiak}
+@opening-prayer{Brother }
 
-@sacrament-hymn{194}
+@event{Ward/Stake Business}
+
+@sacrament-hymn{}
 
 @event{Administration of the Sacrament}
 
-@talk{David and Jessica Pearson}
+@talk{Stephan Short}
+
+@talk{Kaylie Walker Short}
 
 @rest-hymn{209}
 
-@talk{Aaron Felix}
+@talk{Bishop Ben Humpherys}
 
-@event{Ward & Stake Business}
+@closing-hymn{215}
 
-@closing-hymn{206}
-
-@closing-prayer{Sister Jaszkowiak}
+@closing-prayer{Sister }
 
 @section{Announcements}
 
@@ -56,31 +58,18 @@ Our ward uses a Slack workspace to send out announcements about activites, as we
 If you are new to the ward, please help us move your records in by filling out @link["https://forms.gle/bVHiqHTurYz2GYcs5"]{this short form}.
 }
 
-@announcement["Receipts"]{
-If you have receipts to submit, you can do that over the Tools app. If you have any questions, please contact a bishopric member.
-}
-
-@announcement["Christmas program choir"]{
-The 9th and 14th wards are combined for Christmas. Practices for a choir happen after church each Sunday in our chapel.
-
-There is no minimum experience level to sing in the choir—anyone who wants to come is welcome!
-
-The music for the choir is @italic{Come Thou Long Expected Jesus} and you can download the music @link["https://drive.google.com/file/d/1v4B_o7pI0RUpML3S_TQHDBG7LIun9VtT"]{here}.
-}
+@announcement["5th Sunday Second Hour"]{We will be combined meeting in the middle room for the second hour today}
 
 @announcement["Picture for the ward bulletin"]{
 We would love to have a picture of your family for our ward bulletin! Please either bring us a picture yourself, or upload one to @link["https://forms.gle/zDnMhWEGNxgcGupN9"]{this form} and we'll print it for you.
 }
 
-@calendar-event["Christmas Sacrament Meeting" #:date "2024-12-22" #:time "09:00" #:id "christmas-sunday-service" #:hours 1]{
-We will be combined with 14th ward in the East chapel for a special Christmas program. There will be light refreshments after the meeting.
-
-Please take a flyer in the lobby and invite your friends!}
+@announcement["2025 Church Meeting Time"]{We will have Sacrament Meeting at 10:30am stsrting next Sunday}
 
 @section{Upcoming Events}
 
-@calendar-event["Ward temple night" #:date "3rd Wednesday and Friday each month" #:time "19:00"]{
-Ward temple nights are this week, they're on the 3rd Wednesday and Friday of every month at 7:00 PM at the Bountiful temple. Sign up to make an appointment at @link["https://www.churchofjesuschrist.org/temples/schedule/appointment?lang=eng"]{this link}.
+@calendar-event["Ward temple night" #:date "January 15 and 17th" #:time "19:00"]{
+Ward temple nights are on the 3rd Wednesday and Friday of every month at 7:00 PM at the Bountiful temple. Sign up to make an appointment at @link["https://www.churchofjesuschrist.org/temples/schedule/appointment?lang=eng"]{this link}.
 }
 
 @calendar-event["Ward activity" #:date "2025-01-10" 
